00f1ca57966701fcb82c7108bac3181937343b5c
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-05-08 13:11:07 +0900

    初期設定

---
+++ src/test/java/in/tech_camp/shopping_api/ShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.shopping_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
