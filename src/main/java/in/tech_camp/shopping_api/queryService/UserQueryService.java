a8ab347c663083a7cd9646bdc60716385ec4e698
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-05-09 11:24:00 +0900

    IDを使用したユーザー情報取得機能の実装

---
+++ src/main/java/in/tech_camp/shopping_api/queryService/UserQueryService.java
@@ -0,0 +1,20 @@
+package in.tech_camp.shopping_api.queryService;
+
+import org.springframework.stereotype.Service;
+
+import in.tech_camp.shopping_api.entity.UserEntity;
+import in.tech_camp.shopping_api.repository.UserRepository;
+
+@Service
+public class UserQueryService {
+  private final UserRepository userRepository;
+
+  public UserQueryService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public UserEntity getUserById(Integer id) {
+    return userRepository.findById(id).orElse(null);
+  }
+  
+}
---
+++ src/test/java/in/tech_camp/shopping_api/controller/GetUserTest.java
@@ -0,0 +1,64 @@
+package in.tech_camp.shopping_api.controller;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import in.tech_camp.shopping_api.entity.UserEntity;
+import in.tech_camp.shopping_api.factory.UserFactory;
+import in.tech_camp.shopping_api.form.UserForm;
+import in.tech_camp.shopping_api.queryService.UserQueryService;
+
+@WebMvcTest(UserController.class)
+public class GetUserTest {
+  
+  @Autowired
+  MockMvc mockMvc;
+
+  @MockBean
+  UserQueryService userQueryService;
+
+  @Test
+  void ユーザー情報を取得できた場合() throws Exception {
+    UserForm userForm = UserFactory.createDefaultUserForm();
+    UserEntity user = new UserEntity();
+    user.setId(1);
+    user.setUserName(userForm.getUserName());
+    user.setEmail(userForm.getEmail());
+    user.setPassword(userForm.getPassword());
+    user.setPhoneNumber(userForm.getPhoneNumber());
+    user.setPostcode(userForm.getPostcode());
+    user.setAddress(userForm.getAddress());
+    user.setIconImage(userForm.getIconImage());
+    user.setCreatedAt(LocalDateTime.now());
+    user.setUpdatedAt(LocalDateTime.now());
+    user.setDeletedAt(null);
+
+    Mockito.when(userQueryService.getUserById(user.getId())).thenReturn(user);
+
+    mockMvc.perform(get("/users/" + user.getId())
+            .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk());
+  }
+
+  @Test
+  void ユーザー情報を取得できなかった場合() throws Exception {
+    Mockito.when(userQueryService.getUserById(1)).thenReturn(null);
+
+    mockMvc.perform(get("/users/" + 1)
+            .accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isNotFound());
+  }
+}
