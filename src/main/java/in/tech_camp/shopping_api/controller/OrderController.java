3b6ba07181f65e55c3afc389d57720cbb7ed7d0c
masaya-ikeno-ss <masaya.ikeno@smart-group.co.jp>
2025-05-20 16:37:47 +0900

    発送済み機能の実装

--- src/main/java/in/tech_camp/shopping_api/controller/OrderController.java
+++ src/main/java/in/tech_camp/shopping_api/controller/OrderController.java
@@ -109,4 +109,20 @@ public class OrderController {
       return ResponseEntity.badRequest().build();
     }
   }
+
+  @PostMapping("/{orderId}/shipped")
+  public ResponseEntity<Void> orderShipped(@PathVariable Integer orderId) {
+    try {
+      OrderEntity orderEntity = orderQueryService.findById(orderId);
+      if (orderEntity == null) {
+        return ResponseEntity.notFound().build();
+      }
+      orderService.updateShippedStatus(orderEntity);
+      return ResponseEntity.ok().build();
+    } catch (IllegalArgumentException e) {
+      return ResponseEntity.badRequest().build();
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().build();
+    }
+  }
 }
